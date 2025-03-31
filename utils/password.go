1cb059cdab825ee2da4caeb2619cf15528a70ead
Al Fatih Muzakki <malfazakki@gmail.com>
2025-03-31 20:04:46 +0700

    feat(utils): create hashing password and check hashing password function utility

---
+++ utils/password.go
@@ -0,0 +1,15 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+// HashPassword creates a bcrypt hash of the password
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+// CheckPasswordHash compares a password with a hash
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
